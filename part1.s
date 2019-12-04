ee85211028afc620c4ac4f61d6e5376ce1c9417c
Sanjana Jammi <sanjanajammi@gmail.com>
2019-12-04 16:51:29 +0530

    fixing alignment

--- part1.s
+++ part1.s
@@ -49,67 +49,67 @@ Output: .word 0x00;
 
 main:
 
-			LDR R1, =OutFileHandle;         @Printing message to enter array length to stdout
-			LDR R0, [R1];
-	        LDR R1, =arraylen_prompt;	        
-	        SWI SWI_PrStr;
-	        LDR R0, =InFileHandle;			@Reading array length input from stdin
-	        LDR R0, [R0];
-	        SWI SWI_RdInt;
-	        MOV R2, R0; 					@Storing array length in R2
+            LDR R1, =OutFileHandle;         @Printing message to enter array length to stdout
+            LDR R0, [R1];
+            LDR R1, =arraylen_prompt;
+            SWI SWI_PrStr;
+            LDR R0, =InFileHandle;          @Reading array length input from stdin
+            LDR R0, [R0];
+            SWI SWI_RdInt;
+            MOV R2, R0;                     @Storing array length in R2
 
-			LDR R1, =OutFileHandle;			@Printing message to enter array elements to stdout
-			LDR R0, [R1];
-	        LDR R1, =array_prompt;
-	        SWI SWI_PrStr;
-	        
-			MOV R3, R2;						@Using R3 as a loop counter to read all array elements
-			LDR R1, =array;					@Reading array elements one by one
-Loop:   	LDR R0, =InFileHandle;
-	        LDR R0, [R0];
-			SWI SWI_RdInt;
-			STR R0, [R1];					@Storing the array elements read into memory
-			ADD R1, R1, #4;
-			SUBS R3, R3, #1;				@Decrement the loop counter
-			BNE Loop;						@Repeat until all elements (equal to array length) are read
+            LDR R1, =OutFileHandle;         @Printing message to enter array elements to stdout
+            LDR R0, [R1];
+            LDR R1, =array_prompt;
+            SWI SWI_PrStr;
+                    
+            MOV R3, R2;                     @Using R3 as a loop counter to read all array elements
+            LDR R1, =array;                 @Reading array elements one by one
+Loop:       LDR R0, =InFileHandle;
+            LDR R0, [R0];
+            SWI SWI_RdInt;
+            STR R0, [R1];                   @Storing the array elements read into memory
+            ADD R1, R1, #4;
+            SUBS R3, R3, #1;                @Decrement the loop counter
+            BNE Loop;                       @Repeat until all elements (equal to array length) are read
 
-			LDR R1, =OutFileHandle;			@Printing message to enter number to be searched to stdout
-			LDR R0, [R1];
-	        LDR R1, =number_prompt;
-	        SWI SWI_PrStr;
-	        LDR R0, =InFileHandle;			@Reading number to be searched input from stdin
-	        LDR R0, [R0];
-			SWI SWI_RdInt;
-	        MOV R4, R0;  					@Storing number to be searched in R4
+            LDR R1, =OutFileHandle;         @Printing message to enter number to be searched to stdout
+            LDR R0, [R1];
+            LDR R1, =number_prompt;
+            SWI SWI_PrStr;
+            LDR R0, =InFileHandle;          @Reading number to be searched input from stdin
+            LDR R0, [R0];
+            SWI SWI_RdInt;
+            MOV R4, R0;                     @Storing number to be searched in R4
 
-	        LDR R3, =array;					@Register R3 points to the starting memory location of the array
+            LDR R3, =array;                 @Register R3 points to the starting memory location of the array
 
-	        BL SEARCH;						@Call subroutine SEARCH
-	        LDR R1, =Output;
-	        STR R0, [R1];
-	        MOV R2, R0;						@Register R2 has the output
+            BL SEARCH;                      @Call subroutine SEARCH
+            LDR R1, =Output;
+            STR R0, [R1];
+            MOV R2, R0;                     @Register R2 has the output
 
-	        LDR R1, =OutFileHandle;			@Printing result to stdout
-	        LDR R0, [R1];
-	        LDR R1, =result_message;                     
-	        SWI SWI_PrStr;
-	        LDR R1, =OutFileHandle;
-	        LDR R0, [R1];
-	        MOV R1, R2;                                   
-	        SWI SWI_PrInt;
-  			SWI SWI_Exit;					@Stop the program
+            LDR R1, =OutFileHandle;         @Printing result to stdout
+            LDR R0, [R1];
+            LDR R1, =result_message;                     
+            SWI SWI_PrStr;
+            LDR R1, =OutFileHandle;
+            LDR R0, [R1];
+            MOV R1, R2;                                   
+            SWI SWI_PrInt;
+            SWI SWI_Exit;                   @Stop the program
 
 
-SEARCH:	@Subroutine to find the position of element in array(array length and number passed as registers and array elements passed as memory block to subroutine)
-			STMFD SP!, {R5, LR};			
-			EOR R0, R0, R0;
-LOOP:		LDR R5, [R3], #4;				@Looping through all the elements and comparing each one with the number to be searched
-			ADD R0, R0, #1;		  			@R0 holds the resultant position
-			CMP R5, R4;						@If element found, branch to done
-			BEQ DONE;
-			SUBS R2, R2, #1;				@Using array length as a loop counter
-			BNE LOOP;
-			MOV R0, #-1;            		@If all array elements are traversed and number not found, store -1
-DONE:		LDMFD SP!, {R5, PC}				@Return to main program
+SEARCH: @Subroutine to find the position of element in array(array length and number passed as registers and array elements passed as memory block to subroutine)
+            STMFD SP!, {R5, LR};
+            EOR R0, R0, R0;
+LOOP:       LDR R5, [R3], #4;               @Looping through all the elements and comparing each one with the number to be searched
+            ADD R0, R0, #1                  @R0 holds the resultant position
+            CMP R5, R4;                     @If element found, branch to done
+            BEQ DONE;
+            SUBS R2, R2, #1;                @Using array length as a loop counter
+            BNE LOOP;
+            MOV R0, #-1;                    @If all array elements are traversed and number not found, store -1
+DONE:       LDMFD SP!, {R5, PC}             @Return to main program
 
 
--- part2.s
+++ part2.s
@@ -49,95 +49,95 @@ Output: .word 0x00;
 
 main:
 
-			LDR R1, =OutFileHandle;         @Printing message to enter array length to stdout
-			LDR R0, [R1];
-	        LDR R1, =arraylen_prompt;	        
-	        SWI SWI_PrStr;
-	        LDR R0, =InFileHandle;			@Reading array length input from stdin
-	        LDR R0, [R0];
-	        SWI SWI_RdInt;
-	        MOV R2, R0; 					@Storing array length in R2
+            LDR R1, =OutFileHandle;         @Printing message to enter array length to stdout
+            LDR R0, [R1];
+            LDR R1, =arraylen_prompt;
+            SWI SWI_PrStr;
+            LDR R0, =InFileHandle;          @Reading array length input from stdin
+            LDR R0, [R0];
+            SWI SWI_RdInt;
+            MOV R2, R0;                     @Storing array length in R2
 
-			LDR R1, =OutFileHandle;			@Printing message to enter array elements to stdout
-			LDR R0, [R1];
-	        LDR R1, =array_prompt;
-	        SWI SWI_PrStr;
-	        
-			MOV R3, R2;						@Using R3 as a loop counter to read all array elements
-			LDR R1, =array;					@Reading array elements one by one
-Loop:   	LDR R0, =InFileHandle;
-	        LDR R0, [R0];
-			SWI SWI_RdInt;
-			STR R0, [R1];					@Storing the array elements read into memory
-			ADD R1, R1, #4;
-			SUBS R3, R3, #1;				@Decrement the loop counter
-			BNE Loop;						@Repeat until all elements (equal to array length) are read
+            LDR R1, =OutFileHandle;         @Printing message to enter array elements to stdout
+            LDR R0, [R1];
+            LDR R1, =array_prompt;
+            SWI SWI_PrStr;
+                    
+            MOV R3, R2;                     @Using R3 as a loop counter to read all array elements
+            LDR R1, =array;                 @Reading array elements one by one
+Loop:       LDR R0, =InFileHandle;
+            LDR R0, [R0];
+            SWI SWI_RdInt;
+            STR R0, [R1];                   @Storing the array elements read into memory
+            ADD R1, R1, #4;
+            SUBS R3, R3, #1;                @Decrement the loop counter
+            BNE Loop;                       @Repeat until all elements (equal to array length) are read
 
-			LDR R1, =OutFileHandle;			@Printing message to enter number to be searched to stdout
-			LDR R0, [R1];
-	        LDR R1, =number_prompt;
-	        SWI SWI_PrStr;
-	        LDR R0, =InFileHandle;			@Reading number to be searched input from stdin
-	        LDR R0, [R0];
-			SWI SWI_RdInt;
-	        MOV R4, R0;  					@Storing number to be searched in R4
+            LDR R1, =OutFileHandle;         @Printing message to enter number to be searched to stdout
+            LDR R0, [R1];
+            LDR R1, =number_prompt;
+            SWI SWI_PrStr;
+            LDR R0, =InFileHandle;          @Reading number to be searched input from stdin
+            LDR R0, [R0];
+            SWI SWI_RdInt;
+            MOV R4, R0;                     @Storing number to be searched in R4
 
-	        LDR R3, =array;					@Register R3 points to the starting memory location of the array
+            LDR R3, =array;                 @Register R3 points to the starting memory location of the array
 
-	        BL SEARCH;						@Call subroutine SEARCH
-	        LDR R1, =Output;
-	        STR R0, [R1];
-	        MOV R2, R0;						@Register R2 has the output
+            BL SEARCH;                      @Call subroutine SEARCH
+            LDR R1, =Output;
+            STR R0, [R1];
+            MOV R2, R0;                     @Register R2 has the output
 
-	        LDR R1, =OutFileHandle;			@Printing result to stdout
-	        LDR R0, [R1];
-	        LDR R1, =result_message;                     
-	        SWI SWI_PrStr;
-	        LDR R1, =OutFileHandle;
-	        LDR R0, [R1];
-	        MOV R1, R2;                                   
-	        SWI SWI_PrInt;
-  			SWI SWI_Exit;					@Stop the program
+            LDR R1, =OutFileHandle;         @Printing result to stdout
+            LDR R0, [R1];
+            LDR R1, =result_message;                     
+            SWI SWI_PrStr;
+            LDR R1, =OutFileHandle;
+            LDR R0, [R1];
+            MOV R1, R2;                                   
+            SWI SWI_PrInt;
+            SWI SWI_Exit;                   @Stop the program
 
 SEARCH: @Subroutine to find the position of element in array(array length and number passed as registers and array elements passed as memory block to subroutine)
-          	STMFD SP!, {R1, R5-R8, LR};
-          	EOR R5, R5, R5;					@Register R5 holds the min index of the effective array to be searched
-          	MOV R6, R2; 					@Register R6 holds the max index of the effective array to be searched
-			SUB R6, R6, #1;					@Subtracting 1 from max index as array indices start from 0
-          	MOV R7, #4;         			
-LOOP:		SUBS R8, R6, R5;				@Calculating the mid index of the effective array by (max - min)/2+ min
-          	BEQ Skip;						@If min = max, skip the division step
-          	BLT Not_Found;					@If min < max, it indicates that the number is not found, so branch to Not_Found
+            STMFD SP!, {R1, R5-R8, LR};
+            EOR R5, R5, R5;                 @Register R5 holds the min index of the effective array to be searched
+            MOV R6, R2;                     @Register R6 holds the max index of the effective array to be searched
+            SUB R6, R6, #1;                 @Subtracting 1 from max index as array indices start from 0
+            MOV R7, #4;
 
-			MOV R8, R8, LSR #1; 			@Shift right by 1 position to divide by 2 -> (max - min)/2
-			ADD R1, R8, R5;					@Add to min -> (max - min)/2+ min
+LOOP:       SUBS R8, R6, R5;                @Calculating the mid index of the effective array by (max - min)/2+ min
+            BEQ Skip;                       @If min = max, skip the division step
+            BLT Not_Found;                  @If min < max, it indicates that the number is not found, so branch to Not_Found
 
-Skip:		MOV R0, R1;						@R1 holds the mid index
-			ADD R0, R0, #1; 				@Register R0 holds the resultant position, Add 1 to the array index to get the position(as position starts from 1)
-			MUL R8, R7, R1;					@Get the array element at that position by adding 4*(index) to the base address of the array
-			ADD R8, R3, R8;
-			LDR R8, [R8];					@Load the array element at mid index
-            	
-			CMP R4, R8;						@Compare element to be searched with this mid element
-			BEQ DONE;						@If equal, branch to DONE
-			BLT LESS;						@If number is lesser than mid element, look in the first half of the array
-			BGT GREATER;					@If number is greater than mid element, look in the second half of the array
+            MOV R8, R8, LSR #1;             @Shift right by 1 position to divide by 2 -> (max - min)/2
+            ADD R1, R8, R5;                 @Add to min -> (max - min)/2+ min
+
+Skip:       MOV R0, R1;                     @R1 holds the mid index
+            ADD R0, R0, #1;                 @Register R0 holds the resultant position, Add 1 to the array index to get the position(as position starts from 1)
+            MUL R8, R7, R1;                 @Get the array element at that position by adding 4*(index) to the base address of the array
+            ADD R8, R3, R8;
+            LDR R8, [R8];                   @Load the array element at mid index
+            
+            CMP R4, R8;                     @Compare element to be searched with this mid element
+            BEQ DONE;                       @If equal, branch to DONE
+            BLT LESS;                       @If number is lesser than mid element, look in the first half of the array
+            BGT GREATER;                    @If number is greater than mid element, look in the second half of the array
 
 LESS:            
-			SUB R1, R1, #1;					@The effective max index of the new array formed using the first half is mid index -1
-			MOV R6, R1;
-			B LOOP;
+            SUB R1, R1, #1;                 @The effective max index of the new array formed using the first half is mid index -1
+            MOV R6, R1;
+            B LOOP;
 
 GREATER:            
-			ADD R1, R1, #1;					@The effective min index of the new array formed using the second half is mid index + 1
-			MOV R5, R1;            
-			B LOOP;
-			
-Not_Found:	MOV R0, #-1; 			        @If not found, store -1                   
-                       
-DONE:		LDMFD SP!, {R1, R5-R8, PC}      @Return to main program by restoring registers
+            ADD R1, R1, #1;                 @The effective min index of the new array formed using the second half is mid index + 1
+            MOV R5, R1;            
+            B LOOP;
 
-			
+Not_Found:  MOV R0, #-1;                    @If not found, store -1                   
+                       
+DONE:       LDMFD SP!, {R1, R5-R8, PC}      @Return to main program by restoring registers
+
 
 
 
